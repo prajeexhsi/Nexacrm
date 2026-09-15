d038a59575de1b48ac47cbd89d77fce4ebfe3928
prajeexhsi <prajeeshsiju101@gmail.com>
2026-09-15 20:14:44 +0530

    Fix Vercel startup migration bootstrap

--- api/index.py
+++ api/index.py
@@ -1,2 +1,14 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm.settings")
+
+import django
+from django.core.management import call_command
+
+
+django.setup()
+call_command("migrate", run_syncdb=True, verbosity=0, interactive=False)
+
 from crm.wsgi import application
+
 app = application
